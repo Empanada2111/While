923a4120ade9ae201e0089a702f694ad2c75f225
Empanada2111 <139251286+Empanada2111@users.noreply.github.com>
2023-07-18 09:24:02 -0600

    Add files via upload

---
+++ While.cpp
@@ -0,0 +1,16 @@
+//Emilia Avila
+//4to bach en compu
+//WHILE ciclo repetitivo
+#include <iostream>//LIbreria
+using namespace std;//std
+int main() {
+int a,c, respuesta;
+cout<< "ingrese un numero\n";//nos pide el numero inicial
+cin>>a;
+while(c<=100)//si el numero es menor o igual a 100 el ciclo ser[a el siguiente
+{
+    cout<< "ingrese un numero\n";//seguira pidiendo un numero
+    cin>> c;
+}
+return 0;//en dado caso la condicion no se cumpla se acaba el ciclo
+}
